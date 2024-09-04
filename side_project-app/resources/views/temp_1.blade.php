91802245c8cc7163c59815069943dfbcad2af428
Ceasar082201 <refermosoc@gmail.com>
2024-09-04 15:41:58 +0800

    added this->

--- side_project-app/resources/views/temp_1.blade.php
+++ side_project-app/resources/views/temp_1.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div></div>
+</body>
+</html>
--- side_project-app/routes/web.php
+++ side_project-app/routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome');});
+Route::get('/index', function () { return view('temp_1');});
