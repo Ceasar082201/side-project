53a025a08c7739932327353f83f9b9ea4566578b
Ceasar082201 <refermosoc@gmail.com>
2024-09-05 16:59:34 +0800

    removed some and added errors

--- side_project-app/routes/web.php
+++ side_project-app/routes/web.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 //sample_signup&landing page
 
 Route::get('/', function() { return view('welcome'); });
-Route::post('/home', [UI::class, 'create'])->name('create');
-Route::get('/Task', [TaskController::class, 'index'])->name('store');
+Route::post('/home', [UI::class, 'create'])->name('');
+Route::get('/Task', [TaskController::class, 'index'])->name('');
