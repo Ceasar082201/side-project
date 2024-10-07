96c340f0c1e4647975d887a47f0bbd6621404a7b
Ceasar082201 <refermosoc@gmail.com>
2024-10-07 16:12:16 +0800

    added news

--- side_project-app/resources/views/temp_1.blade.php
+++ side_project-app/resources/views/temp_1.blade.php
@@ -6,6 +6,7 @@
     <title>Side-Project</title>
     <link rel="icon" type="image/x-icon" href="{{asset('assets/img/Reborn.png')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -15,17 +16,25 @@
     </header>
     <div class="main-wrapper">
         <div class="sidebar">
-        <a href="http://Facebook.com" target="_blank"><i class="bi bi-facebook"></i>Facebook</a>
-        <br>
-        <a href="http://Youtube.com" target="_blank"><i class="bi bi-youtube"></i> Youtube</a>
-        <br>
-        <a href="http://GooglePlay.com" target="_blank"><i class="bi bi-google-play"></i> Play</a>
-        <br>
-        <a href="http://reddit.com" target="_blank"><i class="bi bi-reddit"></i> Reddit</a>
+        <a href="http://Facebook.com" target="_self"><i class="bi bi-facebook"></i></a>
+        
+        <a href="http://Youtube.com" target="_self"><i class="bi bi-youtube"></i></a>
+        
+        <a href="http://GooglePlay.com" target="_self"><i class="bi bi-google-play"></i></a>
+        
+        <a href="http://reddit.com" target="_self"><i class="bi bi-reddit"></i></a>
     </div>
-    <section>
+    <section class=" container">
         <h3>Welcome Home</h3>
-        a
+        <div class="img-fluid img-thumbnail">
+            <img src="https://wallsdesk.com/wp-content/uploads/2016/11/Google-Images.jpg" alt="Hey Google" width="600" height="400">
+            <img src="https://www.islabit.com/wp-content/uploads/2020/05/logo-windows-update.png" alt="Windows" width="669" height="400">
+            
+        </div>
+        <div class="img-fluid img-thumbnail">
+            <img src="https://www.islabit.com/wp-content/uploads/2020/05/logo-windows-update.png" alt="Windows" width="600" height="400">
+            <img src="https://wallsdesk.com/wp-content/uploads/2016/11/Google-Images.jpg" alt="Hey Google" width="669" height="400">
+        </div>
     </section>
 </body>
 </html>
--- side_project-app/resources/views/welcome.blade.php
+++ side_project-app/resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Side_Project-App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
